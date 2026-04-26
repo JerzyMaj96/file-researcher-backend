5e999b5157bf32224a188361fee60358ed9891e7
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-04-26 13:00:23 +0200

    - feat: add stageUpload method in FileStager service - refactor: change types in StagedUpload record DTO

--- src/main/java/com/jerzymaj/file_researcher_backend/DTOs/StagedUpload.java
+++ src/main/java/com/jerzymaj/file_researcher_backend/DTOs/StagedUpload.java
@@ -1,8 +1,6 @@
 package com.jerzymaj.file_researcher_backend.DTOs;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.UUID;
 
-public record StagedUpload(UUID taskId, Path uploadDir, List<Files> files) {}
+public record StagedUpload(String taskId, Path uploadDir, List<Path> files) {}
--- src/main/java/com/jerzymaj/file_researcher_backend/services/FileStager.java
+++ src/main/java/com/jerzymaj/file_researcher_backend/services/FileStager.java
@@ -5,14 +5,37 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
 @Service
 public class FileStager {
 
     @Value("${storage.upload-dir:temp-uploads}")
     private String storageBaseDir;
 
-    public StagedUpload stageUpload(MultipartFile[] files) {
+    public StagedUpload stageUpload(MultipartFile[] files) throws IOException {
+        String taskId = UUID.randomUUID().toString();
 
+        Path uploadDir = Paths.get(storageBaseDir, taskId).toAbsolutePath();
+        Files.createDirectories(uploadDir);
+
+        List<Path> savedFiles = new ArrayList<>();
+
+        for (MultipartFile file : files) {
+            Path destination = uploadDir.resolve(Objects.requireNonNull(file.getOriginalFilename())).toAbsolutePath();
+            Files.createDirectories(destination.getParent());
+            file.transferTo(destination);
+            savedFiles.add(destination);
+        }
+
+        return new StagedUpload(taskId, uploadDir, savedFiles);
     }
 
 }
