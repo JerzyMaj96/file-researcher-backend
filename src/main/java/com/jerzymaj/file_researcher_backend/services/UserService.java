2907ae68ac142c7488f778ac1ecfc2d61745258c
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-07-13 12:13:14 +0200

    refactor(user service): minor clean-up

--- src/main/java/com/jerzymaj/file_researcher_backend/services/UserService.java
+++ src/main/java/com/jerzymaj/file_researcher_backend/services/UserService.java
@@ -8,8 +8,6 @@ import com.jerzymaj.file_researcher_backend.repositories.UserRepository;
 import com.jerzymaj.file_researcher_backend.security.AuthFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
